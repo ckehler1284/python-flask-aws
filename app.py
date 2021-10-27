2f6be14e7989ac024f5b9870e6b592dbd511549e
Chris Kehler <ck595767@gmail.com>
2021-10-27 18:05:51 -0400

    add time route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -7,6 +8,11 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/time')
+def time():    
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return("Current Time = %s" % current_time)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
